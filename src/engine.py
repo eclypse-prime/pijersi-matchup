fea3e12335a7ad5863b6233eb30deeb62b8cf223
Arthur Liu <arthur.liu.lsh@protonmail.com>
2023-10-03 17:18:46 +0200

    feat: semi-working tournament

--- src/engine.py
+++ src/engine.py
@@ -5,9 +5,10 @@ from typing import Union
 class Engine:
     """A class encapsulating a UGI engine process with methods to send commands and receive stdout."""
 
-    def __init__(self, path: Union[str, bytes, PathLike]):
+    def __init__(self, path: Union[str, bytes, PathLike], name = ''):
         self.loop = asyncio.get_event_loop()
         self.loop.run_until_complete(self.__async_start_process(path))
+        self.name = name
 
     def send_command(self, command: str, expect: str = '', timeout: float = 60) -> list[str]:
         return self.loop.run_until_complete(self.__async_send_command(command, expect, timeout))
--- src/game.py
+++ src/game.py
@@ -1,4 +1,5 @@
 from os import PathLike
+import time
 from typing import Union
 from engine import Engine
 
@@ -6,11 +7,12 @@ from engine import Engine
 class Game:
     """A class encapsulating a match between two engines communicating with the UGI protocol."""
 
-    def __init__(self, engine_path1: Union[str, bytes, PathLike], engine_path2: Union[str, bytes, PathLike]):
-        self.engines = [Engine(engine_path1), Engine(engine_path2)]
+    def __init__(self, engine_path1: Union[str, bytes, PathLike], engine_path2: Union[str, bytes, PathLike], engine_name1: str = '', engine_name2: str = ''):
+        self.engines = (Engine(engine_path1, engine_name1), Engine(engine_path2, engine_name2))
+        self.starting_player = 0
         self.current_player = 0
         self.startpos = 'startpos'
-        self.mode = 'depth 4'
+        self.mode = 'depth 1'
         self.move_list = []
     
     def initialize_engines(self):
@@ -18,6 +20,13 @@ class Game:
             engine.send_command("ugi", "ugiok")
             engine.send_command("isready", "readyok")
             engine.send_command("uginewgame")
+            self.set_pos()
+    
+    def reset(self, starting_player: int):
+        self.starting_player = starting_player
+        self.current_player = starting_player
+        self.move_list = []
+        self.set_pos()
 
     def set_pos(self):
         command = ["position"]
@@ -40,11 +49,18 @@ class Game:
         self.current_player = 1 - self.current_player
     
     def play_until_end(self):
+        command = "query p1turn"
+        winner = 0
+
         game_ended = False
         while not game_ended:
             self.play_move()
             command = "query result"
-            game_state = self.engines[self.current_player].send_command(command, "response")[-1].split(" ")[-1]
+            game_state = self.engines[1 - self.current_player].send_command(command, "response")[-1].split(" ")[-1]
             if game_state != 'none':
-                print(game_state)
                 game_ended = True
+                if game_state == 'draw':
+                    winner = -1
+                else:
+                    winner = 1 - self.current_player
+        return winner
