a044ce10bc3fdb4ab09c3fc400594e66f64f1449
Eclypse-Prime <arthur.liu.lsh@protonmail.com>
2024-09-11 11:12:57 +0200

    feat: added verbose option

--- src/engine.py
+++ src/engine.py
@@ -2,15 +2,19 @@ import asyncio
 from os import PathLike
 from typing import Union
 
+
 class Engine:
     """A class encapsulating a UGI engine process with methods to send commands and receive stdout."""
 
-    def __init__(self, path: Union[str, bytes, PathLike], name = ''):
+    def __init__(self, path: Union[str, bytes, PathLike], name='', verbose=False):
         self.loop = asyncio.get_event_loop()
         self.loop.run_until_complete(self.__async_start_process(path))
         self.name = name
+        self.verbose = verbose
 
     def send_command(self, command: str, expect: str = '', timeout: float = 60) -> list[str]:
+        if self.verbose:
+            print(f">>> {self.name}: {command}")
         return self.loop.run_until_complete(self.__async_send_command(command, expect, timeout))
 
     async def __async_start_process(self, path: Union[str, bytes, PathLike]) -> None:
@@ -30,6 +34,8 @@ class Engine:
                         if expect in line:
                             expect_detected = True
                             break
+        if self.verbose and result != []:
+            print(f"<<< {self.name}: {result}")
         return result
 
     def __del__(self):
--- src/game.py
+++ src/game.py
@@ -8,20 +8,21 @@ class Game:
     """A class encapsulating a match between two engines communicating with the UGI protocol."""
 
     def __init__(self, engine_path1: Union[str, bytes, PathLike], engine_path2: Union[str, bytes, PathLike], engine_name1: str = '', engine_name2: str = ''):
-        self.engines = (Engine(engine_path1, engine_name1), Engine(engine_path2, engine_name2))
+        self.engines = (Engine(engine_path1, engine_name1),
+                        Engine(engine_path2, engine_name2))
         self.starting_player = 0
         self.current_player = 0
         self.startpos = 'startpos'
         self.mode = 'depth 1'
         self.move_list = []
-    
+
     def initialize_engines(self):
         for engine in self.engines:
             engine.send_command("ugi", "ugiok")
             engine.send_command("isready", "readyok")
             engine.send_command("uginewgame")
             self.set_pos()
-    
+
     def reset(self, starting_player: int):
         self.starting_player = starting_player
         self.current_player = starting_player
@@ -43,11 +44,13 @@ class Game:
     def play_move(self):
         self.set_pos()
         command = f"go {self.mode}"
-        move_info = self.engines[self.current_player].send_command(command, "bestmove")
+        move_info = self.engines[self.current_player].send_command(
+            command, "bestmove")
         bestmove = move_info[-1].split(" ")[-1]
         self.move_list.append(bestmove)
+        self.set_pos()
         self.current_player = 1 - self.current_player
-    
+
     def play_until_end(self):
         command = "query p1turn"
         winner = 0
@@ -56,7 +59,8 @@ class Game:
         while not game_ended:
             self.play_move()
             command = "query result"
-            game_state = self.engines[1 - self.current_player].send_command(command, "response")[-1].split(" ")[-1]
+            game_state = self.engines[1 - self.current_player].send_command(
+                command, "response")[-1].split(" ")[-1]
             if game_state != 'none':
                 game_ended = True
                 if game_state == 'draw':
